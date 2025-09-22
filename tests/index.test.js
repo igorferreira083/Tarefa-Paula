31ca1d5f3121d4472a9e24160b61c7dbd9ad0524
igorferreira083 <igorfefesilva12@gmail.com>
2025-09-22 09:40:49 -0300

    agora sim.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,11 @@
-// tests/index.test.js
-const assert = require('assert');
 const soma = require('../src/index');
 
-// Teste 1: 2 + 3 = 5
-assert.strictEqual(soma(2, 3), 5, 'Erro: 2 + 3 deveria ser 5');
+test('2 + 3 deve ser 5', () => {
+  expect(soma(2, 3)).toBe(5);
+});
 
-// Teste 2: 1 + 4 = 5
-assert.strictEqual(soma(1, 4), 5, 'Erro: 1 + 4 deveria ser 5');
+test('1 + 4 deve ser 5', () => {
+  expect(soma(1, 4)).toBe(5);
+});
 
 console.log('Os Testes deram certo');
