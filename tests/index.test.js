e4225c68782b679bd674bfa8ffd893ef8668863c
igorferreira083 <igorfefesilva12@gmail.com>
2025-09-22 09:36:58 -0300

    finalmente.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,11 @@
 // tests/index.test.js
+const assert = require('assert');
 const soma = require('../src/index');
 
-test('soma 1 + 2 para igualar 3', () => {
-  expect(soma(1, 2)).toBe(3);
-});
+// Teste 1: 2 + 3 = 5
+assert.strictEqual(soma(2, 3), 5, 'Erro: 2 + 3 deveria ser 5');
+
+// Teste 2: 1 + 4 = 5
+assert.strictEqual(soma(1, 4), 5, 'Erro: 1 + 4 deveria ser 5');
+
+console.log('Os Testes deram certo');
